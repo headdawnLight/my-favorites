328afbc3899d26f415ca00de2e2ec6dfdd596321
Headdawn Light <sertanix@yandex.com>
2022-08-14 20:13:01 +0300

    Update CardEditForm component and style

--- src/components/CardEditForm.js
+++ src/components/CardEditForm.js
@@ -10,9 +10,11 @@ const CardEditForm = ({ theFav }) => {
 
   const [title, setTitle] = useState(fav.title);
   const [image, setImage] = useState(fav.image);
-  const [items, setItems] = useState(fav.items);
+  const [fav1, setFav1] = useState(fav.fav1);
+  const [fav2, setFav2] = useState(fav.fav2);
+  const [fav3, setFav3] = useState(fav.fav3);
 
-  const updatedFav = { id, title, image, items };
+  const updatedFav = { id, title, image, fav1, fav2, fav3 };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,38 +26,59 @@ const CardEditForm = ({ theFav }) => {
       <Form.Group>
         <Form.Control
           type="text"
-          placeholder="Title *"
+          placeholder="Card title"
           name="title"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
-          required
         />
       </Form.Group>
 
       <Form.Group>
         <Form.Control
           type="text"
-          placeholder="Image link *"
+          placeholder="Card image link"
           name="image"
           value={image}
           onChange={(e) => setImage(e.target.value)}
-          required
+          className="mt-1"
         />
       </Form.Group>
 
       <Form.Group>
         <Form.Control
-          as="textarea"
-          placeholder="Favorite things *"
-          name="items"
-          value={items}
-          onChange={(e) => setItems(e.target.value)}
-          rows={5}
+          type="text"
+          placeholder="First favorite"
+          name="fav1"
+          value={fav1}
+          onChange={(e) => setFav1(e.target.value)}
+          className="mt-1"
         />
       </Form.Group>
 
-      <Button variant="success" type="submit" className="w-100 mt-2" block>
-        Update Your Favorite Card
+      <Form.Group>
+        <Form.Control
+          type="text"
+          placeholder="Second favorite"
+          name="fav2"
+          value={fav2}
+          onChange={(e) => setFav2(e.target.value)}
+          className="mt-1"
+        />
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Control
+          type="text"
+          placeholder="Third favorite"
+          name="fav3"
+          value={fav3}
+          onChange={(e) => setFav3(e.target.value)}
+          className="mt-1"
+        />
+      </Form.Group>
+
+      <Button variant="success" type="submit" className="w-100 mt-3" block>
+        Update Card Information
       </Button>
     </Form>
   );
